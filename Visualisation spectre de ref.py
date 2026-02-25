b152ff0d536e78f1a608e43be08dccfb19eaa0e3
Jonathan Vezina <jovez56@ulaval.ca>
2026-02-25 12:49:05 -0500

    ajout fichers visualisation

---
+++ Visualisation spectre de ref.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+file_path = '/Users/mac/Library/Mobile Documents/com~apple~CloudDocs/Session H26/TPOP Projet 1 /RéfSpectre.TXT'
+data = pd.read_csv(file_path, sep=',', usecols=[1, 2], names=['Pixel', 'Intensity'], header=None)
+
+plt.figure(figsize=(12, 6))
+plt.scatter(data['Pixel'], data['Intensity'], s=1, color='blue', alpha=0.5)
+plt.xlabel('Position (Pixel)')
+plt.ylabel('Intensité (comptes)')
+plt.title('Visualisation du Spectre de Référence')
+plt.savefig('ref_spectre_plot.png')
+plt.show()
